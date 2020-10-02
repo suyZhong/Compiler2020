d7943d48179e6ea1f4749eb9b242832ea3c09305
Kai Ma <ksqsf@mail.ustc.edu.cn>
2020-10-02 17:17:06 +0800

    add tests

--- tests/lab2/test_syntax.sh
+++ tests/lab2/test_syntax.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 CUR_DIR=$(dirname $(realpath $0))
-TESTCASE_DIR=./testcase
-OUTPUT_DIR=./token
+TESTCASE_DIR=$CUR_DIR/testcase
+OUTPUT_DIR=$CUR_DIR/token
 BUILD_DIR=$CUR_DIR/../../build
 
 mkdir -p $OUTPUT_DIR
 
 for testcase in $TESTCASE_DIR/*.cminus
 do
-    $BUILD_DIR/parser < $testcase > $OUTPUT_DIR/$(basename $testcase).tokens
+    filename=$(basename $testcase)
+    $BUILD_DIR/parser < $testcase > $OUTPUT_DIR/${filename%.cminus}.syntax_tree
 done
