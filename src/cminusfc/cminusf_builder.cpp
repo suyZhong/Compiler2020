88ba76a43ea3c72d464f5ff2a204cb983273bd7b
Suyuz <zhongsuyang@mail.ustc.edu.cn>
2020-12-05 21:47:29 +0800

    finish ret, call, and half-alloca

--- src/cminusfc/cminusf_builder.cpp
+++ src/cminusfc/cminusf_builder.cpp
@@ -1,13 +1,17 @@
 #include "cminusf_builder.hpp"
+#include "logging.hpp"
 
 // use these macros to get constant value
 #define CONST_FP(num) \
     ConstantFP::get((float)num, module.get())
 #define CONST_ZERO(type) \
     ConstantZero::get(var_type, module.get())
+#define CONST_INT(num) \
+    ConstantInt::get(num, module.get())
 
 // You can define global variables here
 // to store state
+Value *returnValue;
 
 /*
  * use CMinusfBuilder::Scope to construct scopes
@@ -18,18 +22,44 @@
  */
 
 void CminusfBuilder::visit(ASTProgram &node) {
-    // std::cout << "program" << std::endl;
+    LOG(DEBUG) << "program!";
     for (auto decl : node.declarations) {
         decl->accept(*this);
     }
 }
 
-void CminusfBuilder::visit(ASTNum &node) {}
+void CminusfBuilder::visit(ASTNum &node) {
+    LOG(DEBUG) << "num!";
+    if (node.type == TYPE_INT) {
+        scope.push("intValue",CONST_INT(node.i_val));
+    } else {
+        scope.push("floatValue",CONST_FP(node.f_val));
+    }
+    //ignore other situation
+}
 
-void CminusfBuilder::visit(ASTVarDeclaration &node) {}
+void CminusfBuilder::visit(ASTVarDeclaration &node) {
+    LOG(DEBUG) << "varDec!";
+    bool arrayFlag = false;
+    if (node.num != nullptr) {
+        bool arrayFLag = true;
+        LOG(DEBUG) << "TODO array check";
+        node.num->accept(*this);
+        int arrayLength = node.num->i_val;
+    }
+    Type *varTy;
+    if (node.type == TYPE_INT){
+        varTy = Type::get_int32_type(module.get());
+    } else {
+        varTy = Type::get_float_type(module.get());
+    }
+    auto varAlloca = builder->create_alloca(varTy);
+    scope.push(node.id, varAlloca);
+    LOG(DEBUG) << "is here finished?";
+}
 
 void CminusfBuilder::visit(ASTFunDeclaration &node) {
-    // std::cout << "fundec" << std::endl;
+    LOG(DEBUG) << "funDec!";
     auto TyVoid = Type::get_void_type(module.get());
     auto TyInt32 = Type::get_int32_type(module.get());
     auto TyFloat = Type::get_float_type(module.get());
@@ -72,7 +102,7 @@ void CminusfBuilder::visit(ASTFunDeclaration &node) {
 void CminusfBuilder::visit(ASTParam &node) {}
 
 void CminusfBuilder::visit(ASTCompoundStmt &node) {
-    // std::cout << "comp" << std::endl;
+    LOG(DEBUG) << "compstmt!";
     scope.enter();
     //TODO 在这里要加个bb？
     for (auto decl : node.local_declarations) {
@@ -85,6 +115,7 @@ void CminusfBuilder::visit(ASTCompoundStmt &node) {
 }
 
 void CminusfBuilder::visit(ASTExpressionStmt &node) {
+    LOG(DEBUG) << "expression stmt!";
     if (node.expression != nullptr) {
         node.expression->accept(*this);
     }
@@ -95,18 +126,35 @@ void CminusfBuilder::visit(ASTSelectionStmt &node) {}
 void CminusfBuilder::visit(ASTIterationStmt &node) {}
 
 void CminusfBuilder::visit(ASTReturnStmt &node) {
-    // std::cout << "ret" << std::endl;
-    builder->create_void_ret();
+    LOG(DEBUG) << "ret!";
+    // auto fun = builder->get_insert_block()->get_parent();
+    // auto retBb = BasicBlock::create(module.get(), "", fun);
+    // builder->set_insert_point(retBb);
+    if (node.expression == nullptr) {
+        builder->create_void_ret();
+    } else {
+        node.expression->accept(*this);
+        builder->create_ret(returnValue);
+    }
 }
 
 void CminusfBuilder::visit(ASTVar &node) {}
 
 void CminusfBuilder::visit(ASTAssignExpression &node) {}
 
-void CminusfBuilder::visit(ASTSimpleExpression &node) {}
+void CminusfBuilder::visit(ASTSimpleExpression &node) {
+    if(node.additive_expression_r ==nullptr){
+        //TODO
+    } else {
+        //TODO relops
+    }
+    node.additive_expression_l->accept(*this);
+    if (node.additive_expression_r != nullptr)
+        node.additive_expression_r->accept(*this);
+}
 
 void CminusfBuilder::visit(ASTAdditiveExpression &node) {
-    // std::cout << "add" << std::endl;
+    LOG(DEBUG) << "addi!";
     if (node.additive_expression == nullptr) {
         //把里面东西全加起来 TODO
     } else {
@@ -118,7 +166,7 @@ void CminusfBuilder::visit(ASTAdditiveExpression &node) {
 }
 
 void CminusfBuilder::visit(ASTTerm &node) {
-    // std::cout << "term" << std::endl;
+    LOG(DEBUG) << "term!";
     if (node.term == nullptr) {
         //TODO
     } else {
@@ -130,12 +178,18 @@ void CminusfBuilder::visit(ASTTerm &node) {
 }
 
 void CminusfBuilder::visit(ASTCall &node) {
-    // std::cout << "call" << std::endl;
+    LOG(DEBUG) << "call";
     std::vector<Value *> callParams;
-    //TODO complete callpara
+    auto callFun = scope.find(node.id);
+    Type *callFunTy = callFun->get_type();
+    auto funTy = static_cast<FunctionType *> (callFunTy);
+    //TODO complete int or float
     for (auto arg : node.args) {
+        scope.enter();
         arg->accept(*this);
+        callParams.push_back(scope.find("intValue"));
+        scope.exit();
     }
-    auto calleeFun = scope.find(node.id);
-    auto call = builder->create_call(calleeFun, callParams);
+    auto call = builder->create_call(callFun, callParams);
+    scope.push(node.id, call);
 }
