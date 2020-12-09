1a248740c6c86ac0c1b80e0131091f1e92227d51
Suyuz <zhongsuyang@mail.ustc.edu.cn>
2020-12-09 23:54:12 +0800

    finish expr

--- src/cminusfc/cminusf_builder.cpp
+++ src/cminusfc/cminusf_builder.cpp
@@ -14,9 +14,10 @@
 Value *tmpValue;
 float tmpFloat;
 int tmpInt;
+bool assignFlag;
+Type *paramTy;
 
 //scope has some unique strings
-
 
 /*
  * use CMinusfBuilder::Scope to construct scopes
@@ -94,6 +95,7 @@ void CminusfBuilder::visit(ASTVarDeclaration &node) {
 
 void CminusfBuilder::visit(ASTFunDeclaration &node) {
     //这是函数声明
+    int paraNum = 0;
     LOG(DEBUG) << "funDec!";
     auto TyVoid = Type::get_void_type(module.get());
     auto TyInt32 = Type::get_int32_type(module.get());
@@ -110,8 +112,9 @@ void CminusfBuilder::visit(ASTFunDeclaration &node) {
     }
     std::vector<Type *> funParams;
     for (auto param : node.params) {
-        param->accept(*this);
-        //TODO这里param遍历是干嘛？
+        // param->accept(*this);
+        // funParams.push_back(paramTy);
+        //TODO array pointer
         if (param->isarray) {
             funParams.push_back(Type::get_pointer_type(TyInt32));
         } else {
@@ -129,16 +132,40 @@ void CminusfBuilder::visit(ASTFunDeclaration &node) {
                 break;
             }
         }
+        paraNum++;
     }
     auto funType = FunctionType::get(funTy, funParams);
     auto fun = Function::create(funType, node.id, module.get());
     scope.push(node.id, fun);
     auto bb = BasicBlock::create(module.get(), "", fun);
     builder->set_insert_point(bb);
+    //TODO add args
+    //if not may segfault at call
+    for (auto param : node.params) {
+        param->accept(*this);
+    }
+    
     node.compound_stmt->accept(*this);
 }
 
-void CminusfBuilder::visit(ASTParam &node) {}
+void CminusfBuilder::visit(ASTParam &node) {
+    LOG(DEBUG) << "param";
+    if (node.isarray) {
+        if (node.type == TYPE_INT) {
+            paramTy = Type::get_int32_ptr_type(module.get());
+        } else if (node.type == TYPE_FLOAT)
+            paramTy = Type::get_float_ptr_type(module.get());
+    } else {
+        if (node.type == TYPE_INT)
+            paramTy = Type::get_int32_type(module.get());
+        else if (node.type == TYPE_FLOAT)
+            paramTy = Type::get_float_type(module.get());
+    }
+    LOG(DEBUG) << "alloca?";
+    scope.push(node.id, builder->create_alloca(paramTy));
+    LOG(DEBUG) << "asd?";
+    //TODO array
+}
 
 void CminusfBuilder::visit(ASTCompoundStmt &node) {
     LOG(DEBUG) << "compstmt!";
@@ -188,7 +215,9 @@ void CminusfBuilder::visit(ASTVar &node) {
     if (!varAlloca) {
         LOG(WARNING) << "there's no var";
     }
-    var = builder->create_load(varAlloca);
+    if (!assignFlag) {
+        var = builder->create_load(varAlloca);
+    }
     scope.push("&", varAlloca);
     scope.push("@", var);
     tmpValue = var;
@@ -198,14 +227,23 @@ void CminusfBuilder::visit(ASTVar &node) {
 void CminusfBuilder::visit(ASTAssignExpression &node) {
     Value *varAlloca, *varValue;
     scope.enter();
+    assignFlag = true;
     node.var->accept(*this);
+    assignFlag = false;
     varAlloca = scope.find("&");
     scope.exit();
-    
+    auto varTy = varAlloca->get_type()->get_pointer_element_type();
     scope.enter();
     node.expression->accept(*this);
     varValue = scope.find("@");
     scope.exit();
+    if (!varTy->is_float_type() && varValue->get_type()->is_float_type()) {
+        LOG(DEBUG) << "ohmyzsss";
+        varValue = builder->create_fptosi(varValue, varTy);
+    } else if (!varTy->is_integer_type() && varValue->get_type()->is_integer_type()) {
+        varValue = builder->create_sitofp(varValue, varTy);
+    }
+
     builder->create_store(varValue, varAlloca);
 }
 
@@ -226,21 +264,18 @@ void CminusfBuilder::visit(ASTAdditiveExpression &node) {
     Value *rhs;
     bool opFlag = false;
     LOG(DEBUG) << "addi!";
-    if (node.additive_expression == nullptr) {
-        //把里面东西全加起来 TODO maybe
-    } else {
-        //TODO
-        opFlag = true;
-        if (node.op == OP_PLUS) {
-            // LOG(DEBUG) << "I need to add here";
-            // builder->create_iadd;
-        }
-    }
+    // if (node.additive_expression == nullptr) {
+    //     //nothing TODO
+    // } else {
+    //     //TODO maybe
+    //     opFlag = true;
+    // }
     if (node.additive_expression != nullptr) {
+        opFlag = true;
         scope.enter();
         node.additive_expression->accept(*this);
         //TODO type check
-        LOG(DEBUG) << "run here?";
+        // LOG(DEBUG) << "run here?";
         lhs = scope.find("@");
         scope.exit();
     }
@@ -248,8 +283,28 @@ void CminusfBuilder::visit(ASTAdditiveExpression &node) {
     node.term->accept(*this);
     rhs = scope.find("@");
     scope.exit();
+
+    //TODO fptosi
     if (opFlag) {
-        auto tmp = builder->create_iadd(lhs, rhs);
+        bool isIntOp = true;
+        BinaryInst *tmp;
+        if (isIntOp) {
+            if (node.op == OP_PLUS)
+                tmp = builder->create_iadd(lhs, rhs);
+            else if (node.op == OP_MINUS)
+                tmp = builder->create_isub(lhs, rhs);
+            else {
+                std::abort();
+            }
+        } else {
+            if (node.op == OP_PLUS)
+                tmp = builder->create_fadd(lhs, rhs);
+            else if (node.op == OP_MINUS)
+                tmp = builder->create_fsub(lhs, rhs);
+            else {
+                std::abort();
+            }
+        }
         scope.push("@", tmp);
     } else {
         scope.push("@", rhs);
@@ -258,14 +313,51 @@ void CminusfBuilder::visit(ASTAdditiveExpression &node) {
 
 void CminusfBuilder::visit(ASTTerm &node) {
     LOG(DEBUG) << "term!";
-    if (node.term == nullptr) {
-        //TODO
-    } else {
-        //TODO
-    }
-    if (node.term != nullptr)
+    Value *lhs;
+    Value *rhs;
+    bool opFlag = false;
+    // if (node.term == nullptr) {
+    //     //TODO nothing
+    // } else {
+    //     opFlag = true;
+    // }
+    if (node.term != nullptr) {
+        opFlag = true;
+        scope.enter();
         node.term->accept(*this);
+        lhs = scope.find("@");
+        scope.exit();
+    }
+    scope.enter();
     node.factor->accept(*this);
+    rhs = scope.find("@");
+    scope.exit();
+
+    //TODO type check
+    if (opFlag) {
+        bool isIntOp = true;
+        BinaryInst *tmp;
+        if (isIntOp) {
+            if (node.op == OP_MUL)
+                tmp = builder->create_imul(lhs, rhs);
+            else if (node.op == OP_DIV)
+                tmp = builder->create_isdiv(lhs, rhs);
+            else {
+                std::abort();
+            }
+        } else {
+            if (node.op == OP_MUL)
+                tmp = builder->create_fmul(lhs, rhs);
+            else if (node.op == OP_DIV)
+                tmp = builder->create_fdiv(lhs, rhs);
+            else {
+                std::abort();
+            }
+        }
+        scope.push("@", tmp);
+    } else {
+        scope.push("@", rhs);
+    }
 }
 
 void CminusfBuilder::visit(ASTCall &node) {
