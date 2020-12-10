e4bc9e5c21ac1e4f7a555137a494bc0d11b985cd
Suyuz <zhongsuyang@mail.ustc.edu.cn>
2020-12-10 12:45:35 +0800

    complete all 12 testcases

--- src/cminusfc/cminusf_builder.cpp
+++ src/cminusfc/cminusf_builder.cpp
@@ -14,8 +14,12 @@
 Value *tmpValue;
 float tmpFloat;
 int tmpInt;
+//定义assign flag使得在assign语句取数时，不对var进行load操作
 bool assignFlag;
+//传递param类型，（实际可能并不需要）
 Type *paramTy;
+//如果为真 代表在stmt中遇到了return语句，则ifelse不用前往nextBB
+bool retFlag = false; 
 
 //scope has some unique strings
 
@@ -116,12 +120,13 @@ void CminusfBuilder::visit(ASTFunDeclaration &node) {
         // funParams.push_back(paramTy);
         //TODO array pointer
         if (param->isarray) {
-            funParams.push_back(Type::get_pointer_type(TyInt32));
+            if (param->type == TYPE_INT)
+                funParams.push_back(Type::get_int32_ptr_type(module.get()));
+            else {
+                funParams.push_back(Type::get_float_ptr_type(module.get()));
+            }
         } else {
             switch (param->type) {
-            case TYPE_VOID:
-                funParams.push_back(TyVoid);
-                break;
             case TYPE_INT:
                 funParams.push_back(TyInt32);
                 break;
@@ -132,20 +137,27 @@ void CminusfBuilder::visit(ASTFunDeclaration &node) {
                 break;
             }
         }
-        paraNum++;
     }
     auto funType = FunctionType::get(funTy, funParams);
     auto fun = Function::create(funType, node.id, module.get());
     scope.push(node.id, fun);
+    scope.enter();
     auto bb = BasicBlock::create(module.get(), "", fun);
     builder->set_insert_point(bb);
     //TODO add args
     //if not may segfault at call
+    std::vector<Value *> args; // 获取函数的形参,通过Function中的iterator
+    for (auto arg = fun->arg_begin(); arg != fun->arg_end(); arg++) {
+        args.push_back(*arg); // * 号运算符是从迭代器中取出迭代器当前指向的元素
+    }
     for (auto param : node.params) {
         param->accept(*this);
+        builder->create_store(args[paraNum], scope.find(param->id));
+        paraNum++;
     }
-    
+
     node.compound_stmt->accept(*this);
+    scope.exit();
 }
 
 void CminusfBuilder::visit(ASTParam &node) {
@@ -161,15 +173,13 @@ void CminusfBuilder::visit(ASTParam &node) {
         else if (node.type == TYPE_FLOAT)
             paramTy = Type::get_float_type(module.get());
     }
-    LOG(DEBUG) << "alloca?";
     scope.push(node.id, builder->create_alloca(paramTy));
-    LOG(DEBUG) << "asd?";
     //TODO array
 }
 
 void CminusfBuilder::visit(ASTCompoundStmt &node) {
     LOG(DEBUG) << "compstmt!";
-    scope.enter();
+    //把scope放到了funDeclar里面
     //TODO 在这里要加个bb？
     for (auto decl : node.local_declarations) {
         decl->accept(*this);
@@ -177,7 +187,6 @@ void CminusfBuilder::visit(ASTCompoundStmt &node) {
     for (auto stmt : node.statement_list) {
         stmt->accept(*this);
     }
-    scope.exit();
 }
 
 void CminusfBuilder::visit(ASTExpressionStmt &node) {
@@ -187,12 +196,51 @@ void CminusfBuilder::visit(ASTExpressionStmt &node) {
     }
 }
 
-void CminusfBuilder::visit(ASTSelectionStmt &node) {}
+void CminusfBuilder::visit(ASTSelectionStmt &node) {
+    bool isNext = false;
+    auto fun = builder->get_insert_block()->get_parent();
+    auto trueBB = BasicBlock::create(module.get(), "", fun);
+    auto falseBB = BasicBlock::create(module.get(), "", fun);
+    BasicBlock *nextBB;
+    scope.enter();
+    node.expression->accept(*this);
+    auto cond = scope.find("@");
+    scope.exit();
+    //TODO maybe the answer is not i1 ??
+    //TODO this func is totally ugly 
+    //i want to make it elegant
+    builder->create_cond_br(cond, trueBB, falseBB);
+    builder->set_insert_point(trueBB);
+    node.if_statement->accept(*this);
+    if (!retFlag && node.else_statement !=nullptr)
+    {
+        nextBB = BasicBlock::create(module.get(), "", fun);
+        builder->create_br(nextBB);
+        isNext = true;
+    } else if (node.else_statement == nullptr) {
+        builder->create_br(falseBB);
+    }
+    retFlag = false;
+        builder->set_insert_point(falseBB);
+    if (node.else_statement != nullptr) {
+        node.else_statement->accept(*this);
+        if (!retFlag) {
+            if (!isNext)
+                nextBB = BasicBlock::create(module.get(), "", fun);
+            builder->create_br(nextBB);
+            isNext = true;
+        }
+    }
+    retFlag = false;
+    if (isNext)
+        builder->set_insert_point(nextBB);
+}
 
 void CminusfBuilder::visit(ASTIterationStmt &node) {}
 
 void CminusfBuilder::visit(ASTReturnStmt &node) {
     LOG(DEBUG) << "ret!";
+    retFlag = true;
     // auto fun = builder->get_insert_block()->get_parent();
     // auto retBb = BasicBlock::create(module.get(), "", fun);
     // builder->set_insert_point(retBb);
@@ -248,14 +296,80 @@ void CminusfBuilder::visit(ASTAssignExpression &node) {
 }
 
 void CminusfBuilder::visit(ASTSimpleExpression &node) {
+    Value *lhs;
+    Value *rhs;
+    bool opFlag = false;
+    bool isIntOp = true;
+    LOG(DEBUG) << "simp expr";
     if (node.additive_expression_r == nullptr) {
         //TODO maybe
     } else {
         //TODO relops must
     }
+    scope.enter();
     node.additive_expression_l->accept(*this);
+    lhs = scope.find("@");
+    scope.exit();
     if (node.additive_expression_r != nullptr) {
+        scope.enter();
         node.additive_expression_r->accept(*this);
+        rhs = scope.find("@");
+        scope.exit();
+        if (isIntOp) {
+            CmpInst *tmp;
+            switch (node.op) {
+            case OP_LE:
+                tmp = builder->create_icmp_le(lhs, rhs);
+                break;
+            case OP_LT:
+                tmp = builder->create_icmp_lt(lhs, rhs);
+                break;
+            case OP_GT:
+                tmp = builder->create_icmp_gt(lhs, rhs);
+                break;
+            case OP_GE:
+                tmp = builder->create_icmp_ge(lhs, rhs);
+                break;
+            case OP_EQ:
+                tmp = builder->create_icmp_eq(lhs, rhs);
+                break;
+            case OP_NEQ:
+                tmp = builder->create_icmp_ne(lhs, rhs);
+                break;
+            default:
+                std::abort();
+                break;
+            }
+            scope.push("@", tmp);
+        } else {
+            FCmpInst *tmp;
+            switch (node.op) {
+            case OP_LE:
+                tmp = builder->create_fcmp_le(lhs, rhs);
+                break;
+            case OP_LT:
+                tmp = builder->create_fcmp_lt(lhs, rhs);
+                break;
+            case OP_GT:
+                tmp = builder->create_fcmp_gt(lhs, rhs);
+                break;
+            case OP_GE:
+                tmp = builder->create_fcmp_ge(lhs, rhs);
+                break;
+            case OP_EQ:
+                tmp = builder->create_fcmp_eq(lhs, rhs);
+                break;
+            case OP_NEQ:
+                tmp = builder->create_fcmp_ne(lhs, rhs);
+                break;
+            default:
+                std::abort();
+                break;
+            }
+            scope.push("@", tmp);
+        }
+    } else {
+        scope.push("@", lhs);
     }
 }
 
@@ -263,6 +377,7 @@ void CminusfBuilder::visit(ASTAdditiveExpression &node) {
     Value *lhs;
     Value *rhs;
     bool opFlag = false;
+    bool isIntOp = true;
     LOG(DEBUG) << "addi!";
     // if (node.additive_expression == nullptr) {
     //     //nothing TODO
@@ -286,7 +401,7 @@ void CminusfBuilder::visit(ASTAdditiveExpression &node) {
 
     //TODO fptosi
     if (opFlag) {
-        bool isIntOp = true;
+        isIntOp = true;
         BinaryInst *tmp;
         if (isIntOp) {
             if (node.op == OP_PLUS)
@@ -376,6 +491,7 @@ void CminusfBuilder::visit(ASTCall &node) {
         callParams.push_back(scope.find("@"));
         scope.exit();
     }
+    LOG(DEBUG) << "setbreakpoint";
     auto call = builder->create_call(callFun, callParams);
-    scope.push(node.id, call);
+    scope.push("@", call);
 }
